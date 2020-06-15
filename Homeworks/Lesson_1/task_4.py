b68d09e5c64bbf83ee10d00120ed9ccefc950c64
Sergey Aleshin <ageresnishela@gmail.com>
2020-06-15 22:43:48 +0300

    task_4 correct

--- Homeworks/Lesson_1/task_4.py
+++ Homeworks/Lesson_1/task_4.py
@@ -3,7 +3,25 @@
 while True:
     number = input('Введи целое положительное число:\n')
     if number.isdigit():
+        number = int(number)
         break
     else:
         print('Нужно ввести целое положительное число!')
-print('Самая большая цифра:\n', max(number))
+
+result = 0
+while number and result != 9:
+    new_num = number % 10
+    print(new_num)
+    if new_num > result:
+        result = new_num
+    number//=10
+print(result)
+
+result = 9
+
+while number:
+    if str(result) in str(number):
+        print(result)
+        break
+    else:
+        result -= 1
