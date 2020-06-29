e2d8f7d20c1823dccd3b95690fbf0e676c13d6f9
Sergey Aleshin <ageresnishela@gmail.com>
2020-06-29 22:42:34 +0300

    HW lesson 5

--- Homeworks/task_5.py
+++ Homeworks/task_5.py
@@ -1,12 +1,18 @@
 '''5. Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами.
 Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.'''
-from functools import reduce
 
-def summator(prev_el, el):
-    return float(prev_el) + float(el)
+import random
 
-with open('task_5_data.txt') as file:
-    numbers = file.read().split(' ')
+numbers = [random.randint(1,100) for _ in range(random.randint(1,100))]
+summ = sum(numbers)
 
-summa = reduce(summator, numbers)
-print(f'Сумма чисел == {summa}')
+with open('task_5_data.txt', 'w', encoding='UTF-8') as file:
+    str_data = map(str, numbers)
+    file.write(' '.join(str_data))
+
+with open('task_5_data.txt', 'r', encoding='UTF-8') as file_2:
+    data = file_2.read()
+    int_data = map(int, data.split(' '))
+    summa = sum(int_data)
+
+print(f'Сумма == {summa}')
